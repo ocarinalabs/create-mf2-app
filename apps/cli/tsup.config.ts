8e7f2eda9803c4d6d2673ba6757f804feb385e7f
faw <fawwad30@gmail.com>
2026-02-22 19:38:17 +0800

    feat(cli): add new cli app with build system

---
+++ apps/cli/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["scripts/index.ts"],
+  outDir: "dist",
+  sourcemap: false,
+  minify: true,
+  dts: false,
+  format: ["esm"],
+  banner: { js: "#!/usr/bin/env node" },
+});
