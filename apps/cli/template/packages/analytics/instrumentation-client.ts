ae95dc671e0fedc00a115b9f8eecd0e77aa751bb
faw <fawwad30@gmail.com>
2026-03-15 10:00:00 +0800

    refactor(analytics): add graceful degradation

--- apps/cli/template/packages/analytics/instrumentation-client.ts
+++ apps/cli/template/packages/analytics/instrumentation-client.ts
@@ -2,13 +2,13 @@ import posthog from "posthog-js";
 import { keys } from "./keys";
 
 export const initializeAnalytics = () => {
-  const key = keys().NEXT_PUBLIC_POSTHOG_KEY;
-  if (!key) {
+  const { NEXT_PUBLIC_POSTHOG_KEY, NEXT_PUBLIC_POSTHOG_HOST } = keys();
+  if (!(NEXT_PUBLIC_POSTHOG_KEY && NEXT_PUBLIC_POSTHOG_HOST)) {
     return;
   }
 
-  posthog.init(key, {
-    api_host: keys().NEXT_PUBLIC_POSTHOG_HOST,
+  posthog.init(NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: NEXT_PUBLIC_POSTHOG_HOST,
     defaults: "2025-05-24",
   });
 };
--- apps/cli/template/packages/analytics/server.ts
+++ apps/cli/template/packages/analytics/server.ts
@@ -2,12 +2,15 @@ import "server-only";
 import { PostHog } from "posthog-node";
 import { keys } from "./keys";
 
-const key = keys().NEXT_PUBLIC_POSTHOG_KEY;
+const { NEXT_PUBLIC_POSTHOG_KEY, NEXT_PUBLIC_POSTHOG_HOST } = keys();
 
-export const analytics = key
-  ? new PostHog(key, {
-      host: keys().NEXT_PUBLIC_POSTHOG_HOST,
-      flushAt: 1,
-      flushInterval: 0,
-    })
-  : undefined;
+export const analytics =
+  NEXT_PUBLIC_POSTHOG_KEY && NEXT_PUBLIC_POSTHOG_HOST
+    ? new PostHog(NEXT_PUBLIC_POSTHOG_KEY, {
+        host: NEXT_PUBLIC_POSTHOG_HOST,
+
+        // Don't batch events and flush immediately - we're running in a serverless environment
+        flushAt: 1,
+        flushInterval: 0,
+      })
+    : undefined;
