699a117025871c1131010c816849360d6cb9f099
faw <fawwad30@gmail.com>
2026-03-15 13:30:00 +0800

    refactor(design-system): switch to radix icons in mode toggle

--- apps/cli/template/packages/design-system/components/mode-toggle.tsx
+++ apps/cli/template/packages/design-system/components/mode-toggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Moon, Sun } from "lucide-react";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
 import { Button } from "../components/ui/button";
 import {
@@ -27,12 +27,12 @@ export const ModeToggle = () => {
           size="icon"
           variant="ghost"
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
+      <DropdownMenuContent>
         {themes.map(({ label, value }) => (
           <DropdownMenuItem key={value} onClick={() => setTheme(value)}>
             {label}
